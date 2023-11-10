89ac6646a81c3ee4d8211c29c12e423294cf48c1
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-11-10 12:05:08 -0500

    feat: :sparkles: Add DbSet for all entities in File Context
    @Gonther1

--- Persistencia/Data/ApiFarmaciaContext.cs
+++ Persistencia/Data/ApiFarmaciaContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Dominio.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistencia.Data;
@@ -16,6 +17,24 @@ public class ApiFarmaciaContext : DbContext
     public ApiFarmaciaContext(DbContextOptions options) : base(options)
     {
     }
+    public DbSet<Ciudad> Ciudades { get; set; }
+    public DbSet<ContactoPersona> ContactosPersonas { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<DetalleMovInventario> DetallesMovInventarios { get; set; }
+    public DbSet<Factura> Facturas { get; set; }
+    public DbSet<FormaPago> FormasPagos { get; set; }
+    public DbSet<Inventario> Inventarios { get; set; }
+    public DbSet<Marca> Marcas { get; set; }
+    public DbSet<MovimientoInventario> MovimientosInventarios { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Persona> Personas { get; set; }
+    public DbSet<Presentacion> Presentaciones { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<RolPersona> RolesPersonas { get; set; }
+    public DbSet<TipoContacto> TiposContactos { get; set; }
+    public DbSet<TipoDocumento> TiposDocumentos { get; set; }
+    public DbSet<TipoMovInventario> TiposMovInventarios { get; set; }
+    public DbSet<UbicacionPersona> UbicacionesPersonas { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
