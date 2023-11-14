a5076e02dcdaf702e96c4db529c8fef9699cf228
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-11-13 19:53:57 -0500

    feat: Added Service Files
    @Gonther1

---
+++ ApiFarmacia/Services/IUserService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiFarmacia.Dtos;
+
+namespace ApiFarmacia.Services;
+
+public interface IUserService
+{
+    Task<string> RegisterAsync(RegisterDto model);
+    Task<DataUserDto> GetTokenAsync(LoginDto model);
+    Task<string> AddRoleAsync(AddRoleDto model);
+    Task<DataUserDto> RefreshTokenAsync(string refreshToken);
+}
